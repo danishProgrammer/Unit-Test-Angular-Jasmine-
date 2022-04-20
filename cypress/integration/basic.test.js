a9bb752dfa6e14284423c0d6843f1541a3a25da9
Nityanand Rai <n.n.r1204@gmail.com>
2022-04-20 11:51:40 +0530

    contain edit and delete on hover - Nityanand

--- cypress/integration/basic.test.js
+++ cypress/integration/basic.test.js
@@ -1,68 +1,49 @@
+import "cypress-real-events/support";
+
 describe('basic comp',()=>{
     beforeEach(()=>{
-        cy.fixture('course.json').as('courseJSON')
-        cy.server()
+        cy.fixture('course.json').as('courseJSON');
+        cy.server();
         cy.route('http://localhost:4200/assets/courses.json','@courseJSON').as('course')
         cy.visit("/");
     })
     it("should display All cources",()=>{
-        // expect(true).to.equal(false)
+        
         cy.contains("Welcome to Angular Unit Testing Practice Course")
         cy.wait('@course')
-        cy.log('created new user')
-        cy.get('@course').then((courses)=>{
-            cy.get("app-course").should("have.length",Object.keys(courses).length)
-            cy.log(courses);
+        cy.get('@course').then((res)=>{
+            const courses  = res.response.body;
+            cy.get("app-course").should("have.length",courses.length);
+           
+            console.log(courses.filter(course => course.courseName.includes('Maths')));
         })
         
     })
-    it('should type and show result',()=>{
-        cy.get(".search").should("have.length",1)
-        cy.get(".search").type("Maths")
-        cy.get("app-course").should("have.length",1)
-    })
     it('should open Add course',()=>{
-        cy.get(".addCourse").should("have.length",1)
-        cy.get(".addCourse").first().click()
-        cy.url().should('include', '/new-course') 
-        cy.contains("Course Name")
-        cy.contains("Passing Marks")
-        cy.contains("Total Marks")
-   })
-   it('should Add course',()=>{
-    cy.visit("/new-course");
-    
-    cy.contains("Course Name")
-    cy.contains("Passing Marks")
-    cy.get("#courseName").type("EVS")
-    cy.get("#passingMarks").type("60")
-    cy.get("#totalMarks").type("100")
-    cy.get('.submit').click()
-   })
-   
-   it('change color on hover',()=>{
-    //cy.get('.course:hover').first().trigger('mouseup').should('have.css', 'background-color', 'rgba(39, 75, 5)');
-    // cy.wait(10000)
-    // .should('have.css', 'background-color', '-rgb(39, 75, 5)');
-    //cy.get('.course').first().trigger('mouseup').should('have.css', 'background-color', 'rgb(39, 75, 5)');
-    
-    // cy.get('.course').first().trigger('hold')
-    // cy.get('.course:hover').first()
-    // .should('have.css', 'background-color', 'rgb(39, 75, 5)');
-})
+         cy.get(".addCourse").should("have.length",1)
+         cy.get(".addCourse").first().click()
+         cy.contains("Course Name")
+        //   cy.get(".form-group #courseId").type("11")
+        //  cy.get(".courseName").type("EVS")
+        //  cy.get(".courseMarks").type("60")
+        //  cy.get(".courseTotalMarks").type("100")
+        // cy.get(".task").should("contain","This is task page.")
+    })
+    it('should type and show result',()=>{
+        cy.get(".search").should("have.length",1);
+        cy.get(".search").type("Maths");
+        cy.get('@course').then((res)=>{
+        const courses  = res.response.body;
+        cy.get("app-course").should("have.length",courses.filter(course => course.courseName.includes('Maths')).length);
+    });
+});
 
-
-
-it('should click edit , navigate to edit and add data and save', ()=>{
-    cy.get(".edit").first().click({ force: true })
-    cy.contains("Course Name")
-    cy.contains("Passing Marks")
-    cy.contains("Total Marks")
-    cy.url().should('include', '/1/edit') 
-    cy.get("#courseName").clear().type("Maths")
-    cy.get("#passingMarks").clear().type("40")
-    cy.get("#totalMarks").clear().type("100")
-    cy.get('.submit').click()
-})
-
+    it('should contain edit and delete button on hover', ()=>{
+        cy.get('.course').eq(3).as('thirdEl');
+        cy.get('@thirdEl').realHover().should('have.css', 'background-color', 'rgb(39, 75, 5)');
+        cy.get('@thirdEl').realHover().contains('Edit');
+        cy.get('@thirdEl').realHover().find('.course-actions .btn').should('have.length', 2);
+        cy.wait(3000);
+        // cy.get('@thirdEl').realHover().find('.course-actions:visible .btn').contains('Edit').click();
+    });
 })
